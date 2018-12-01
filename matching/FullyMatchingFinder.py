2dcf9dda997fc851d034167dcc128ceb6a1b9b09
Denis <aksion2011@yandex.ru>
2018-12-02 03:35:36 +0500

    backup

--- matching/FullyMatchingFinder.py
+++ matching/FullyMatchingFinder.py
@@ -13,15 +13,13 @@ def generate_adjency_matrix():
                 s = next(file)
             except StopIteration:
                 break
-        header = adj_array[:4]
+        header = adj_array[:int(len_x)+1]
         bias_adj_ranges = [header[i+1]-header[i] for i in range(len(header)-1)]
         adj_mat = [[0 for g in range(int(len_y))] for i in range(int(len_x))]
         for head_index in range(len(header) - 1):
             for bias in range(bias_adj_ranges[head_index]):
                 adj_mat[head_index][adj_array[header[head_index]-1+bias]-1] = -1
-        # todo 15
-        print(adj_mat)
-        return adj_array
+        return adj_mat
 
 
 def write_result(lst):
