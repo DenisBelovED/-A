91e56bad2cba8b22503fec2c5c83b8262a8a16e4
Denis <aksion2011@yandex.ru>
2018-12-13 03:41:17 +0500

    backup

--- matching/FullyMatchingFinder.py
+++ matching/FullyMatchingFinder.py
@@ -15,8 +15,8 @@ def generate_adjency_matrix():
                 s = next(file)
             except StopIteration:
                 break
-        header = adj_array[:len_x+1]
-        bias_adj_ranges = [header[i+1]-header[i] for i in range(len(header)-1)]
+        header = adj_array[:len_x + 1]
+        bias_adj_ranges = [header[i + 1] - header[i] for i in range(len(header) - 1)]
         adj_mat = [[0 for j in range(len_y)] for i in range(len_x)]
         for head_index in range(len(header) - 1):
             for bias in range(bias_adj_ranges[head_index]):
@@ -37,22 +37,20 @@ def select_free_vertex(adj_mat, x_i, res):
     return -1
 
 
+def create_mt(adj_mat, res, x_i):
+    y_i = adj_mat[x_i].index(1)  # возьмём первую попавшуюся вершину y_i
+    x_j = res.index(y_i)  # будем смотреть, какой х_j с ней связан
+
+
 # метод делает проверку для x, существует ли свободная вершина в Y
 # если да, то result[x] = y
 # иначе начинает строить увеличивающуюся M-цепь
 def dfs_x(adj_mat, temp_res, x_i):
-    if temp_res[x_i] == -1: # проверяем, есть ли паросочетание
-        y_i = select_free_vertex(adj_mat, x_i, temp_res) # выбираем свободный y
+    if temp_res[x_i] == -1:  # проверяем, есть ли паросочетание
+        y_i = select_free_vertex(adj_mat, x_i, temp_res)  # выбираем свободный y
         if y_i == -1:
             # свободного y нет, строим увеличивающуюся M-цепь
-            # возьмём первую попавшуюся вершину
-            y = -1
-            for y_i in range(len(adj_mat[x_i])):
-                if adj_mat[x_i][y_i] == 1:
-                    y = y_i
-                    break
-            # будем смотреть, какой х с ней связан
-
+            mt_chain = []
         else:
             # свободный y есть, заносим его в ответ, и возвращаем изменённый массив паросочетаний
             temp_res[x_i] = y_i
@@ -60,7 +58,6 @@ def dfs_x(adj_mat, temp_res, x_i):
     else:
         # паросочетание есть, ничего не изменяем
         return temp_res
-
 
 
 def find_matching(adj_mat):
