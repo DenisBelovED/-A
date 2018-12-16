f38516334fbf7e32d062a4d8881ee0723fdf7b19
Denis <aksion2011@yandex.ru>
2018-12-16 19:19:11 +0500

    backup

--- AssignmentTask/AssignmentTask.py
+++ AssignmentTask/AssignmentTask.py
@@ -1,5 +1,22 @@
+def generate_adjency_matrix():
+    with open('in.txt', 'r') as file:
+        next(file)
+        adj_mat = [list(map(int, s.split())) for s in file]
+    return adj_mat
+
+
+def write_result(res):
+    with open('out.txt', 'w') as file:
+        file.write(str(res))
+
+
+def find_max_profit(adj_mat):
+    return adj_mat
+
+
 def main():
-    pass
+    adj_mat = generate_adjency_matrix()
+    write_result(find_max_profit(adj_mat))
 
 
 if __name__ == '__main__':
